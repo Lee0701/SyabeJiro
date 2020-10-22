63988f6da2f9f80d3f6a2985a7a55ddfd2878022
Lee0701 <hgl1002@naver.com>
2020-10-22 18:06:29 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+
+require('dotenv').config()
+const Discord = require('discord.js')
+const {speak} = require('./papago-tts.js')
+
+const client = new Discord.Client()
+
+const token = process.env.BOT_TOKEN
+const prefix = '2?'
+
+const channels = {}
+
+const commands = {
+    join: (args, msg) => {
+        const member = msg.guild.members.resolve(msg.author)
+        const voice = member.voice
+        if(!voice || !voice.channel) return
+        voice.channel.join().then((connection) => {
+            channels[msg.channel.id] = {
+                channel: msg.channel,
+                connection: connection,
+                queue: []
+            }
+        })
+    },
+    leave: (args, msg) => {
+        const member = msg.guild.members.resolve(msg.author)
+        const voice = member.voice
+        if(!voice || !voice.channel) return
+        voice.channel.leave()
+        if(channels[msg.channel.id]) delete channels[msg.channel.id]
+    },
+}
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}.`)
+})
+
+client.on('message', (msg) => {
+    if(msg.author.bot) return
+    if(!msg.content) return
+
+    const channel = channels[msg.channel.id]
+    if(channel) {
+        channel.queue.push(msg.content)
+        const speakAuto = (text) => new Promise((resolve, reject) => {
+            const speaker = 'yuri'
+            speak(text, speaker).then((url) => {
+                const dispatcher = channel.connection.play(url)
+                dispatcher.on('finish', () => {
+                    resolve()
+                })
+            })
+        })
+        const speakNext = () => {
+            speakAuto(channel.queue[0]).then(() => {
+                channel.queue.shift()
+                if(channel.queue.length > 0) speakNext()
+            })
+        }
+        if(channel.queue.length === 1) speakNext()
+    }
+
+    if(msg.content.startsWith(prefix)) {
+        const args = msg.content.slice(prefix.length).split(/\s+/)
+        const cmd = args.shift()
+        const command = commands[cmd]
+        if(command) command(args, msg)
+    }
+})
+
+client.login(token)
---
+++ papago-tts.js
@@ -0,0 +1,60 @@
+
+const p = require('phin')
+
+const crypto = require('crypto')
+const {v4: uuidv4} = require('uuid')
+
+let uuid = null
+let key = ''
+
+const updateKey = () => {
+  key = process.env.PAPAGO_VERSION || 'v1.5.2_0d13cb6cf4'
+  uuid = uuidv4()
+}
+
+const generateAuthorization = (baseUrl) => {
+  const timestamp = new Date().getTime()
+  const hmac = crypto.createHmac('md5', key)
+  hmac.update(`${uuid}\n${baseUrl}\n${timestamp}`)
+  const token = hmac.digest('base64')
+  const authorization = `PPG ${uuid}:${token}`
+  return {timestamp, authorization}
+}
+
+const makeID = (text, speaker) => new Promise((resolve, reject) => {
+  const baseUrl = 'https://papago.naver.com/apis/tts/makeID'
+  const generateOptions = () => {
+    const {timestamp, authorization} = generateAuthorization(baseUrl)
+    return {
+      url: baseUrl,
+      method: 'POST',
+      headers: {
+        'User-Agent': 'Mozilla/5.0',
+        'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Authorization': authorization,
+        'Timestamp': timestamp,
+      },
+      form: {
+        alpha: '0',
+        pitch: '0',
+        speaker: speaker,
+        speed: '0',
+        text: text,
+      },
+    }
+  }
+  p(generateOptions()).then((res) => {
+    if(res.statusCode === 403) {
+      updateKey()
+      p(generateOptions()).then(r => JSON.parse(r.body.toString()).id).catch(reject)
+    } else {
+      resolve(JSON.parse(res.body.toString()).id)
+    }
+  }).catch(reject)
+})
+
+const speak = (text, speaker) => new Promise((resolve, reject) => {
+  makeID(text, speaker).then((id) => resolve(`https://papago.naver.com/apis/tts/${id}`)).catch(reject)
+})
+
+module.exports = {makeID, speak}
